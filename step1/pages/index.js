ae1cb264e71e0e805551fe1addafe9a0c20ccf93
Kanan Rahimov <mail@kenanbek.me>
2020-11-09 18:52:22 +0100

    step1: manual setup and public folder

---
+++ step1/pages/index.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>Next.js quick start tutorial with manual setup.</p>
+      <img src="/image1.png" />
+    </div>
+  );
+}
+
+export default HomePage;
